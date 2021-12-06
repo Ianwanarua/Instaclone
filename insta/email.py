aeb7d3360711f87067f43fd9531a8026878a0e16
ianwanarua <ian.wanarua@student.moringaschool.com>
2021-12-06 09:51:51 +0300

    changed the message text

--- insta/email.py
+++ insta/email.py
@@ -2,7 +2,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
-  subject = 'Welcome to Insta clone by denis'
+  subject = 'Welcome to Insta clone'
   sender = 'denismugendinjue9@gmail.com'
   text_content = render_to_string('email/welcome.txt',{"name":name})
   html_content = render_to_string('email/welcome.html',{"name":name})
