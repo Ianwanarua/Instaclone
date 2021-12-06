7195411eb56472967dc6aea2723cedbebb54d4a2
ianwanarua <ian.wanarua@student.moringaschool.com>
2021-12-06 09:57:21 +0300

    Updated the deployment

--- insta/forms.py
+++ insta/forms.py
@@ -27,7 +27,7 @@ class CommentForm(forms.ModelForm):
   def __init__(self,*args,**kwargs):
     super().__init__(*args,**kwargs)
     self.fields['comment'].widget=forms.TextInput()
-    self.fields['comment'].widget.attrs['placeholder']='Add a comment...'
+    self.fields['comment'].widget.attrs['placeholder']='Add a comment..'
   class Meta:
     model = Comment
     fields = ('comment',)
